a3dad2530660d6e6fa4209d0bda45cfb1ca51386
cadorn <christoph@christophdorn.com>
2013-02-28 16:59:26 -0800

    parse query

--- app/nodejs/server.js
+++ app/nodejs/server.js
@@ -4,6 +4,7 @@ const CONNECT = require("connect");
 const PINF_IT = require("pinf-it");
 
 var app = CONNECT()
+    .use(CONNECT.query())
     .use(CONNECT.bodyParser())
 	.use(PINF_IT.SERVER.for(module).connect())
     .listen(8080);
